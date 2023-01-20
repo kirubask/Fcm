1f4ca0d552a170a7c52df07c1eeca76650d50578
Anilkumar <anilkumar@aitechindia.com>
2023-01-20 14:19:59 +0530

    final commit

--- app/src/main/java/com/example/fcm/MainActivity.java
+++ app/src/main/java/com/example/fcm/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         titlee = findViewById(R.id.titleEditTxt);
         messagee = findViewById(R.id.messageEditTxt);
         send =findViewById(R.id.sendBtn);
